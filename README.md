0dca85381b38c173b08f5d8e3897297440cb8c4f
zhulu <shaonianzhentan@sina.cn>
2021-07-09 22:07:48 +0800

    测试效果

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# node-red-contrib-fetch
-node-red里使用node-fetch
+# node-red-contrib-input-event
+node-red里使用监听键盘按键输入
 
 
 ```bash
 # 切换到.node-red里使用
-npm i https://github.com/shaonianzhentan/node-red-contrib-fetch
+npm i https://github.com/shaonianzhentan/node-red-contrib-input-event
 ```
---
+++ input-event.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const _ = require('lodash');
+const InputEvent = require('input-event');
+
+module.exports = function (RED) {
+
+    RED.nodes.registerType("input-event", (config) => {
+
+        RED.nodes.createNode(this, config);
+
+        const { inputDevice } = config
+        if (!fs.existsSync(inputDevice)) {
+            return this.status({ fill: "red", shape: "ring", text: `未找到设备：${inputDevice}` });
+        }
+        const input = new InputEvent(inputDevice);
+        const keyboard = new InputEvent.Keyboard(input);
+        //keyboard.on('keyup'   , console.log);
+        //keyboard.on('keydown' , console.log);
+        keyboard.on('keypress', _.throttle((data) => {
+            this.send({
+                dev: inputDevice,
+                code: data.code
+            })
+        }), 500);
+    });
+}
