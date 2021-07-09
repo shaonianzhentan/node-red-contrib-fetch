5fa2d6e8aae47e47e74af404b0fe752f27b7ccc5
zhulu <shaonianzhentan@sina.cn>
2021-07-09 23:33:13 +0800

    功能完成

--- input-event.js
+++ input-event.js
@@ -5,21 +5,27 @@ const InputEvent = require('input-event');
 module.exports = function (RED) {
 
     function NodeInputEvent(config) {
+        const node = this
         RED.nodes.createNode(this, config);
 
         const inputDevice = config.name
         if (!fs.existsSync(inputDevice)) {
             return this.status({ fill: "red", shape: "ring", text: `未找到设备：${inputDevice}` });
         }
+        // console.log(inputDevice)
         const input = new InputEvent(inputDevice);
         const keyboard = new InputEvent.Keyboard(input);
         //keyboard.on('keyup'   , console.log);
         //keyboard.on('keydown' , console.log);
-        keyboard.on('keypress', _.throttle((data) => {
-            this.send({
-                dev: inputDevice,
-                code: data.code
-            })
+        keyboard.on('keypress', _.throttle(function (data) {
+            node.status({ fill: "green", shape: "ring", text: `键码：${data.code}` });
+            // console.log(data)
+            node.send([{
+                payload: {
+                    dev: inputDevice,
+                    ...data
+                }
+            }])
         }), 500);
     }
     RED.nodes.registerType("input-event", NodeInputEvent);
